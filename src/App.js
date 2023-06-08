1e40e97151db235aa6305dc1e25b77f189c09532
PrachiRameshGodase <prachigodase2@gmail.com>
2023-06-08 23:45:08 +0530

    when use login in dirrect go to product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import {useState} from "react"
+import {useState,useContext} from "react"
 import './App.css';
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,Navigate  } from 'react-router-dom';
 import Cart from "./components/Cart/Cart"
 import Header from './components/Layout/Header';
 import AvailableProducts from './components/Products/AvailableProducts';
@@ -12,10 +12,12 @@ import Home from "./components/Pages/Home";
 import ContactUs from "./components/Pages/ContactUs";
 import ProductDetails from "./components/Products/ProductDetails";
 import LoginForm from "./components/Pages/LoginForm";
+import AuthContext from "./store/auth-context";
 
 function App() {
-
+  // const Navigate=useNavigate()
   const [cartIsShown, setCartIsShown]=useState(false);
+  const authCtx=useContext(AuthContext);
 
   const showCartHandler=()=>{
     setCartIsShown(true);
@@ -33,13 +35,16 @@ function App() {
       <Header onShowCart={showCartHandler}/>
       <Routes>
       
-      <Route path="/" element={<Home/>} />
+      {/* <Route path="/" element={<Home/>} /> */}
       <Route path="/about" element={<About />} />
       <Route path="/contact_us" element={< ContactUs/>} />
-      <Route path="/login" element={< LoginForm/>} />
-      <Route path="/product" element={<AvailableProducts/>} />
-      <Route path="/product/:imageUrl" element={<ProductDetails/>}/>
+      {/* <Route path="/login" element={< LoginForm/>} />
+      <Route path="/product" element={<AvailableProducts/>} /> */}
+      {authCtx.isLoggedIn ?  <Route path="/product" element={<AvailableProducts />} /> : <Route path="/product" element={<LoginForm />} />}
 
+      (<Route path="/login" element={<LoginForm />} />) 
+      <Route path="/product/:imageUrl" element={<ProductDetails/>}/>
+      <Route path="/" element={<Home/>} />
         </Routes>
        
         
